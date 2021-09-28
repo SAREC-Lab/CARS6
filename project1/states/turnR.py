01dc71afd3f4f60d98ace8be76cda1ca73e253ed
SquidneySquush <64729009+SquidneySquush@users.noreply.github.com>
2021-09-28 19:57:13 -0400

    Update turnR.py

--- project1/states/turnR.py
+++ project1/states/turnR.py
@@ -13,5 +13,15 @@ class TurnR(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('The turtle is turning right')
-	time.sleep(2)
+	v, delta = float(1), float(-0.9) # negative angle turns right
+	dur = rospy.Duration(1.0)
+    	rate = rospy.Rate(10)
+    	start = rospy.Time.now()
+	
+	drive = AckermannDrive(steering_angle=delta, speed=v)
+
+    	while rospy.Time.now() - start < dur:
+		pub_controls.publish(AckermannDriveStamped(drive=drive))
+		rate.sleep()
+		
         return 'do_plan'
