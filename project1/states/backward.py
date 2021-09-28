f7482b829660c424e3b5b6a13b2c4bd2a99f6342
efredin1 <78491631+efredin1@users.noreply.github.com>
2021-09-28 19:46:42 -0400

    Update backward.py

--- project1/states/backward.py
+++ project1/states/backward.py
@@ -9,22 +9,38 @@ from geometry_msgs.msg import Twist
 class Forward(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['do_plan'])
-        self.counter = 0
+        self.counter = 0 
+        self.pub_init_pos = pub_init_pos
+        self.pub_controls = pub_controls
 
     def execute(self, userdata):
+      # get state attributes
+        state_name = userdata.curr_state["name"]
+        distance = userdata.curr_state["attributes"]["distance"]
 
-        velocity_publisher = rospy.Publisher(', Twist, queue_size=10)
+        rospy.loginfo("Running {} state".format(state_name))
+        
+       # set initial state position
+        send_init_pos(state_name, self.pub_init_pos)
 
-        t0=rospy.Time.now().to_sec()
-        t1=rospy.Time.now().to_sec()
+        velocity = 2.0  # default velocity
 
-        while(t1-t0<2):
+        dur = rospy.Duration(10)
+        rate = rospy.Rate(10)
+        
+        start = rospy.Time.now()
+        end = rospy.Time.now().to_sec()
+        
+        drive = AckermannDrive(steering_angle=delta, speed=velocity)
+       
+        while(end - start < distance):
+            self.pub_controls.publish(AckermannDriveStamped(drive=drive))
+            rate.sleep()
+           
             #Publish the velocity
             velocity_publisher.publish(vel_msg)
-            t1=rospy.Time.now().to_sec()          
-         
-        #After the loop, stops the robot
-        vel_msg.linear.x = 0
-        #Force the robot to stop
-        velocity_publisher.publish(vel_msg)
+               
+        time.sleep(1)
+        
+        
         return 'do_plan'
