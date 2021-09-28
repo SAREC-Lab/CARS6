5338c7d618c2718286252bfa462864cc34c58440
SquidneySquush <64729009+SquidneySquush@users.noreply.github.com>
2021-09-28 17:49:17 -0400

    Update turnL.py

---
+++ project1/states/turnL.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+import smach
+import time
+from geometry_msgs.msg import Twist
+
+# define state turnL - turn left
+class TurnL(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['do_plan'])
+        self.counter = 0
+
+    def execute(self, userdata):
+        rospy.loginfo('The turtle is turning left')
+	time.sleep(2)
+        return 'do_plan'
