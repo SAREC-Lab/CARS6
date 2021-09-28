5e7a52615f8314191ca65cc2f148470b7074904a
SquidneySquush <64729009+SquidneySquush@users.noreply.github.com>
2021-09-28 19:55:49 -0400

    Update turnL.py

--- project1/states/turnL.py
+++ project1/states/turnL.py
@@ -4,6 +4,7 @@ import rospy
 import smach
 import time
 from geometry_msgs.msg import Twist
+from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped
 
 # define state turnL - turn left
 class TurnL(smach.State):
@@ -13,5 +14,16 @@ class TurnL(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('The turtle is turning left')
-	time.sleep(2)
+	#time.sleep(2)
+	v, delta = float(1), float(0.9)
+	dur = rospy.Duration(1.0)
+    	rate = rospy.Rate(10)
+    	start = rospy.Time.now()
+	
+	drive = AckermannDrive(steering_angle=delta, speed=v)
+
+    	while rospy.Time.now() - start < dur:
+		pub_controls.publish(AckermannDriveStamped(drive=drive))
+		rate.sleep()
+	
         return 'do_plan'
