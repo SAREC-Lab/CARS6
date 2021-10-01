f62f25d96f197dc49b5bfff51b90cf8814801ab7
SquidneySquush <64729009+SquidneySquush@users.noreply.github.com>
2021-10-01 14:40:22 -0400

    update.py

--- project1/states/turnL.py
+++ project1/states/turnL.py
@@ -8,7 +8,7 @@ from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped
 
 # define state turnL - turn left
 class TurnL(smach.State):
-    def __init__(self):
+    def __init__(self, pub_controls):
         smach.State.__init__(self, outcomes=['do_plan'])
         self.counter = 0
 	
@@ -20,14 +20,14 @@ class TurnL(smach.State):
         rospy.loginfo("Running {} state".format(state_name))
 	#time.sleep(2)
 	v, delta = float(1), float(0.9)
-	dur = rospy.Duration(1.0)
+	dur = rospy.Duration(2.0)
     	rate = rospy.Rate(10)
     	start = rospy.Time.now()
 	
 	drive = AckermannDrive(steering_angle=delta, speed=v)
 
     	while rospy.Time.now() - start < dur:
-		pub_controls.publish(AckermannDriveStamped(drive=drive))
+		self.pub_controls.publish(AckermannDriveStamped(drive=drive))
 		rate.sleep()
 	
         return 'do_plan'
