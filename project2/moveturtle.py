965f23e7ca988a26bc7d0e40977d7066840134d4
efredin1 <78491631+efredin1@users.noreply.github.com>
2021-10-27 17:23:09 -0400

    Update moveturtle.py

--- project2/moveturtle.py
+++ project2/moveturtle.py
@@ -5,3 +5,14 @@ from sensor_msgs.msg import LaserScan
 class Turtlebot():
 
     def __init__(self, namespace=""):
+        self.namespace = namespace
+        self.x = 0
+        self.y = 0
+        self.angle = 0
+        self.LIDAR_ERR = 0.05
+        self.rate = rospy.Rate(10)
+
+        # Register publishers and subscribers
+        self.publisher = rospy.Publisher(self.namespace + '/cmd_vel', Twist, queue_size=1)
+
+        rospy.on_shutdown(self.shutdown)
