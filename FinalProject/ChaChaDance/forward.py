17224e07f7f3bff2bd85312a0716c2f869febe68
Your Name <shnat@nd.edu>
2021-12-01 17:50:44 -0500

    add clap state and edits

---
+++ FinalProject/ChaChaDance/forward.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Import libraries
+import rospy
+import smach
+import time
+import math
+from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped
+
+# Define circle state
+
+
+class Forward(smach.State):
+    def __init__(self, pub_controls):
+        smach.State.__init__(self,
+                             outcomes=["do_plan"],
+                             input_keys=["curr_state"])
+        self.counter = 0
+        self.pub_controls = pub_controls
+
+
+    def execute(self, userdata):
+        # get state attributes
+        state_name = userdata.curr_state["name"]
+
+        rospy.loginfo("Running {} state".format(state_name))
+
+        while rospy.Time.now() - start < dur:
+            self.pub_controls.publish(AckermannDriveStamped(drive=drive))
+            rate.sleep()
+
+        time.sleep(1)
+
+        return "do_plan"
