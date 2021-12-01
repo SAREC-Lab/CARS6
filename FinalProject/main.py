02291147371bcad80380ef6e1f9d2ccacc9cb829
SquidneySquush <64729009+SquidneySquush@users.noreply.github.com>
2021-12-01 02:33:03 -0500

    Update main.py

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -2,18 +2,45 @@
 
 # Import libraries
 import rospy
+import smach
+import time
+from geometry_msgs.msg import Twist
 from moveturtle import Turtlebot
 from maze_navigator import navigate
+#Add states to library
+
+
+# define state Stop
+class Stop(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['do_exit'])
+        self.counter = 0
+
+    def execute(self, userdata):
+        rospy.loginfo('The turtle has stopped')
+	time.sleep(1)
+        return 'do_exit'
 
 
 # Run bot
 def runTurtleBot():
     rospy.init_node('squishy_turtlebot3')
     turtleBot = Turtlebot(namespace="squishy")
+    
+     # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['do_exit'])
 
-    navigate(turtleBot)
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('TwistRight', TwistRight(), 
+                               transitions={'do_twist_left':'TwistLeft'})
 
-    rospy.spin()
+    #navigate(turtleBot)
+
+    #rospy.spin()
+    # Execute SMACH plan
+    outcome = sm.execute()
 
 
 if __name__ == '__main__':
