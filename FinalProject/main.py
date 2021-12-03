5dbb158483339af021b21a470d28a0dfaaa2baa6
Gerry Migwi <23432496+gerry101@users.noreply.github.com>
2021-12-02 20:02:20 -0500

    Update main.py

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
 # Import libraries
+from ChaCha.chacha import Chacha
 import rospy
 import smach
 import time
+import json
+from plan_parser import parse_plan
 from geometry_msgs.msg import Twist
-from moveturtle import Turtlebot
-from maze_navigator import navigate
-#Add states to library
-from ChaChaDance.forward import Forward
-from ChaChaDance.backward import Backward
-from ChaChaDance.circle import Circle
-from ChaChaDance.clap import Clap
-from ChaChaDance.chacha import Chacha
-from ChaChaDance.turnL import TurnL
-from ChaChaDance.turnR import TurnR
-from ChaChaDance.plan import Plan
+# Add states to library
+from ChaCha.forward import Forward
+from ChaCha.backward import Backward
+from ChaCha.circle import Circle
+from ChaCha.clap import Clap
+from ChaCha.turnL import TurnL
+from ChaCha.turnR import TurnR
 
 
 # define state Stop
@@ -26,47 +25,52 @@ class Stop(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('The turtle has stopped')
-	time.sleep(1)
+        time.sleep(1)
         return 'do_exit'
 
-
 # Run bot
-def runTurtleBot():
+
+
+def runTurtleBot(plan):
     rospy.init_node('squishy_turtlebot3')
     turtleBot = Turtlebot(namespace="squishy")
-    
-     # Create a SMACH state machine
+
+    # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['do_exit'])
+    sm.userdata.plan = plan
+    sm.userdata.plan_length = len(plan)
+    sm.userdata.curr_state = {}
 
     # Open the container
     with sm:
         # Add states to the container
-        smach.StateMachine.add('Plan', Plan(), 
-                               transitions={'do_forward':'Forward', 'do_backward': 'Backward', 'do_circle':'Circle', 
-					    'do_turnL':'TurnL', 'do_turnR':'TurnR','do_clap':'Clap','do_chacha':'Chacha','do_exit':'Stop'})
-        smach.StateMachine.add('Forward', Forward(), 
-                               transitions={'do_plan':'Plan'})
-        smach.StateMachine.add('Backward', Backward(), 
-                                transitions={'do_plan':'Plan'})
-        smach.StateMachine.add('Circle', Circle(), 
-                               transitions={'do_plan':'Plan'})
-        smach.StateMachine.add('TurnL', TurnL(), 
-                               transitions={'do_plan':'Plan'})
-        smach.StateMachine.add('TurnR', TurnR(), 
-                               transitions={'do_plan':'Plan'})
-	smach.StateMachine.add('Clap', Clap(), 
-                               transitions={'do_clap':'Clap'})
-	smach.StateMachine.add('Chacha', Chacha(), 
-                               transitions={'do_chacha':'Chacha'})
-        smach.StateMachine.add('Stop', Stop(), 
-                               transitions={'do_exit':'do_exit'})
+        smach.StateMachine.add('Plan', Plan(),
+                               transitions={'do_forward': 'Forward', 'do_backward': 'Backward', 'do_circle': 'Circle',
+                                            'do_turnL': 'TurnL', 'do_turnR': 'TurnR', 'do_clap': 'Clap', 'do_chacha': 'Chacha', 'do_exit': 'Stop'})
+        smach.StateMachine.add('Forward', Forward(),
+                               transitions={'do_plan': 'Plan'})
+        smach.StateMachine.add('Backward', Backward(),
+                               transitions={'do_plan': 'Plan'})
+        smach.StateMachine.add('Circle', Circle(),
+                               transitions={'do_plan': 'Plan'})
+        smach.StateMachine.add('TurnL', TurnL(),
+                               transitions={'do_plan': 'Plan'})
+        smach.StateMachine.add('TurnR', TurnR(),
+                               transitions={'do_plan': 'Plan'})
+        smach.StateMachine.add('Clap', Clap(),
+                               transitions={'do_clap': 'Clap'})
+        smach.StateMachine.add('Chacha', Chacha(),
+                               transitions={'do_chacha': 'Chacha'})
+        smach.StateMachine.add('Stop', Stop(),
+                               transitions={'do_exit': 'do_exit'})
 
-    #navigate(turtleBot)
+    # navigate(turtleBot)
 
-    #rospy.spin()
+    # rospy.spin()
     # Execute SMACH plan
     outcome = sm.execute()
 
 
 if __name__ == '__main__':
-    runTurtleBot()
+    plan = parse_plan("FinalProject/plans/plan2.json")
+    runTurtleBot(plan)
